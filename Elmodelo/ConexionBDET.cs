007178d9a75111abc30650a734825950c0736dcf
iamtelles <112676652+iamtelles@users.noreply.github.com>
2023-10-10 00:01:55 -0600

    Add files via upload

---
+++ Elmodelo/ConexionBDET.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using MySql.Data.MySqlClient;
+namespace web_umg_bd
+{
+    public class ConexionBD
+    {
+        private string cadena = "server=localhost; database=db_empresa; user=User_empre_tare7; password=edgarte@123";
+        public MySqlConnection conectar = new MySqlConnection();
+        
+
+        public void AbrirConexion(){
+            try {
+                conectar.ConnectionString = cadena;
+                conectar.Open();
+               // System.Diagnostics.Debug.WriteLine("Conexion Exitosa");
+               
+            } 
+            catch(Exception ex){
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+               // Console.WriteLine(ex.StackTrace);
+                
+            }
+
+        }
+        public void CerarConexion(){
+            
+                if (conectar.State == ConnectionState.Open){
+                conectar.Close();            }
+
+        }
+    }
+}
